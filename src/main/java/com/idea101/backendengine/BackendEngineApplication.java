f71034499b6bb9a4a5c78489c7b4783f266d199f
svpritish <svpritish2002@gmail.com>
2025-09-19 01:06:55 +0530

    initial empty template

---
+++ src/main/java/com/idea101/backendengine/BackendEngineApplication.java
@@ -0,0 +1,13 @@
+package com.idea101.backendengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idea101/backendengine/BackendEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea101.backendengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
